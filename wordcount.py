5e0e80f768a22bcfe5c9f0e05814b8d18b31b459
yueyang0115 <44473421+yueyang0115@users.noreply.github.com>
2021-03-13 23:27:52 -0500

    Update wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -22,16 +22,15 @@ from pyspark.sql import SparkSession
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: wordcount <file>", file=sys.stderr)
-        sys.exit(-1)
-
+    
+    inputFile = "s3://wordcountdata/input.txt"
+    
     spark = SparkSession\
         .builder\
         .appName("PythonWordCount")\
         .getOrCreate()
 
-    lines = spark.read.text(sys.argv[1]).rdd.map(lambda r: r[0])
+    lines = spark.read.text(inputFile).rdd.map(lambda r: r[0])
     counts = lines.flatMap(lambda x: x.split(' ')) \
                   .map(lambda x: (x, 1)) \
                   .reduceByKey(add)
